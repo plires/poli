b5e2e2414d0f5ef6ca789b9d257f43a02f3dcd53
Pablo Lires <pablolires@gmail.com>
2022-04-29 13:01:53 -0300

    eliminacion de whatsapp en header

--- includes/header.php
+++ includes/header.php
@@ -27,11 +27,6 @@
 		          	target="_blank" 
 		          	href="<?= RRSS_INSTAGRAM ?>"><i class="transition fa-brands fa-instagram-square"></i>
 		          </a>
-		          <a 
-		          	rel="noopener" 
-		          	target="_blank" 
-		          	href="https://web.whatsapp.com/send?phone=+<?= WHATSAPP ?>&text=Hola%20Superfil,%20Necesito%20hacer%20una%20consulta...%20"><i class="transition fa-brands fa-whatsapp-square"></i>
-		          </a>
 		        </div>
 
 						<nav>
